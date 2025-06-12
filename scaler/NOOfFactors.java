f47116c371af9e41b5d836c5fb01ee5fe2e79e37
Ritik-se <ritikagrawal2705@gmail.com>
2025-06-12 22:35:12 +0530

    finding the no of factors

---
+++ scaler/NOOfFactors.java
@@ -0,0 +1,14 @@
+// https://www.scaler.com/academy/mentee-dashboard/class/34598/assignment/problems/29028/submissions//
+public class Solution {
+    int count;
+    public int solve(int A) {
+        for(int i=1;i<=A;i++)
+        {
+            if(A%i==0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
