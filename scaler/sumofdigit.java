e02bed80fd4082af0122dc55e25d6e6d98ccd93f
Ritik-se <ritikagrawal2705@gmail.com>
2025-08-08 22:19:57 +0530

    Create sumofdigit.java
    it. is basically sum of digits

---
+++ scaler/sumofdigit.java
@@ -0,0 +1,13 @@
+
+public public class Solution {
+
+    public int solve(int A) {
+        if (A == 0) {
+            return 0;
+        }
+        return (A % 10) + solve(A / 10);
+    }
+}
+{
+    
+}
