6ba5a06bf051a1924b568c946d2cb13acf409e04
Ritik-se <ritikagrawal2705@gmail.com>
2025-08-08 12:18:09 +0530

    Create noccurence.java

---
+++ scaler/noccurence.java
@@ -0,0 +1,26 @@
+
+public class Solution {
+
+    public int getSum(int c, int B, ArrayList<Integer> A) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.size(); i++) {
+
+            map.put(A.get(i), map.getOrDefault(A.get(i), 0) + 1);
+
+        }
+        int sum = 0;
+        boolean bol = false;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == B) {
+                bol = true;
+
+                sum += entry.getKey();
+            }
+        }
+
+        if (bol) {
+            return sum % 1000000007;
+        }
+        return -1;
+
+    }
