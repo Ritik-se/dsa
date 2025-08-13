31c1ef6a2ee78a8e7cbd0c77164b46774a290708
Ritik-se <ritikagrawal2705@gmail.com>
2025-08-13 19:03:04 +0530

    Create patterns.java

---
+++ scaler/patterns.java
@@ -0,0 +1,22 @@
+
+public class patterns {
+
+    public static void main(String[] args) {
+        pat(4);
+
+    }
+
+    public static void pat(int n) {
+        for (int row = 0; row < n; row++) {
+            {
+                for (int col = 0; col <= i; col++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+
+            }
+
+        }
+    }
+
+}
