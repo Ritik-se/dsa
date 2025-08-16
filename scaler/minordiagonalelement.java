d99efa6cc124a5e8b3767a2a08867727f1668cfb
Ritik-se <ritikagrawal2705@gmail.com>
2025-08-16 13:16:10 +0530

    dsa update
    update

--- scaler/minordiagonalelement.java
+++ scaler/minordiagonalelement.java
@@ -5,6 +5,7 @@ public class Solution {
     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
     public int solve(final int[][] A) {
         int sum = 0;
+        //length
         int n = A.length;
         for (int i = 0; i < n; i++) {
             sum += A[i][n - 1 - i];//imp for j
