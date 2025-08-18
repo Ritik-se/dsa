0ed0a0b30147deb544f0a9778efac2862ec0199d
Ritik-se <ritikagrawal2705@gmail.com>
2025-08-18 13:27:13 +0530

    Create secondfreq.java

---
+++ scaler/secondfreq.java
@@ -0,0 +1,43 @@
+public import java.util.*;
+class Solution {
+    public int secondMostFrequentElement(int[] nums) {
+      int result=Integer.MAX_VALUE;
+      if(nums==null||nums.length==0) return -1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        int smax=-1,max=-1;
+       for(Map.Entry<Integer,Integer> entry:map.entrySet())
+       {
+        if(max<entry.getValue())
+        {
+            smax=max;
+            max=entry.getValue();
+        }
+        else if(entry.getValue()>smax&&entry.getValue()<max)
+        {
+            smax=entry.getValue();
+        }
+       
+
+       }
+        if(smax==-1) return -1;
+       for(Map.Entry<Integer,Integer> entry:map.entrySet())
+       {
+        if(entry.getValue()==smax)
+        {
+            result=Math.min(result,entry.getKey());
+        }
+       }
+       return result;
+
+
+     
+    }
+}
+
+ {
+    
+}
