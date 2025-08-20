8a59f40b31f740520646ac0a0e5eaef3b284012e
Ritik-se <ritikagrawal2705@gmail.com>
2025-08-20 19:35:26 +0530

    Update palindrome.java

--- scaler/palindrome.java
+++ scaler/palindrome.java
@@ -4,8 +4,9 @@ public public class Solution {
     public int palindrome(int i, int j, String A) {
         if (i >= j) {
             return 1;
+            // base condition
         }
-        if (A.charAt(i) != A.charAt(j)) {
+        if (A.charAt(i) != A.charAt(j)) {// checks if char have been diffrent 
             return 0;
         }
         return palindrome(i + 1, j - 1, A);
