62c5aa33921d8c1aca0158f0b0ac74b7b135a908
Ritik-se <ritikagrawal2705@gmail.com>
2025-08-22 16:54:49 +0530

    Create pickfrombothside.java

---
+++ scaler/pickfrombothside.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int solve(int[] A, int B) {
+        int curr=0;
+        for(int i=0;i<B;i++)
+        {
+            curr+=A[i];
+        }
+        int max=curr;
+        int rem=B-1,add=A.length-1;
+        while(rem>=0&&add>=0)
+        {
+            curr+=A[add--];
+            curr-=A[rem--];
+            max=Math.max(max,curr);
+        }
+        return max;
+    }
+}
