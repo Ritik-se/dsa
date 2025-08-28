0d06afb7132d3e329426cec6dc26f75ff5111503
Ritik-se <ritikagrawal2705@gmail.com>
2025-08-28 19:04:23 +0530

    Update sumofdiagonalelement.java

--- scaler/sumofdiagonalelement.java
+++ scaler/sumofdiagonalelement.java
@@ -6,7 +6,7 @@ public class Solution {
     public int solve(final int[][] A) {
         int sum = 0;
         for (int i = 0; i < A.length; i++) {
-            sum += A[i][i];
+            sum += A[i][i];// row=coloum
         }
         return sum;
     }
