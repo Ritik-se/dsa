7fee3152cb84aba0addfd03b2229e44eaee622d8
Ritik-se <ritikagrawal2705@gmail.com>
2025-09-02 21:44:22 +0530

    Create logicofadditionin.java

---
+++ scaler/logicofadditionin.java
@@ -0,0 +1,17 @@
+
+int carry=0;
+        for(int i=maxlength-1;i>=0;i--)
+        {
+            int bit1=0,bit2=0;
+        if(A.charAt(i)==1)
+        {
+            bit1=1;
+        }
+        if(B.charAt(i)==1)
+        {
+            bit2=1;
+        }
+        int sum=bit1+bit2+carry;
+        int result=sum%2;/// logic and easy 
+         carry=sum/2;
+         ans.append(result);
