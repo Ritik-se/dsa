a668d0483a612da064dc0a2704b27de8524c535f
Ritik-se <ritikagrawal2705@gmail.com>
2025-09-09 05:58:40 +0530

    Update swappinginacedingorder.java

--- scaler/swappinginacedingorder.java
+++ scaler/swappinginacedingorder.java
@@ -1,25 +1,24 @@
+
 public public class Solution {
+
     public int solve(int[] A) {
-        int swaps=0;
-        for(int i=0;i<A.length;)
-        { int temp=0;
-            if(A[i]!=i)
-            {
-                temp=A[i];
-                A[i]=A[A[i]];
-                A[temp]=temp;//ON
+        int swaps = 0;
+        for (int i = 0; i < A.length;) {
+            int temp = 0;
+            if (A[i] != i) {
+                temp = A[i];
+                A[i] = A[A[i]];
+                A[temp] = temp;//ON
                 swaps++;
-      
-                
-            }
-            if(A[i]==i)
-            {
-              i++;
+
+            }// also called reverse
+            if (A[i] == i) {
+                i++;
             }
         }
         return swaps;
     }
 }
- {
+{
     
 }
