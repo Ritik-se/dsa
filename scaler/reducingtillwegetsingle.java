e7677d1c7cf6fb749c163154bb6e414ebc2fefe6
Ritik-se <ritikagrawal2705@gmail.com>
2025-09-17 00:24:52 +0530

    Create reducingtillwegetsingle.java

---
+++ scaler/reducingtillwegetsingle.java
@@ -0,0 +1,27 @@
+
+public class Solution {
+
+    public int addDigits(int num) {
+        if (num <= 9) {
+            return num;
+        }
+        int ans = summission(num, 0);
+        return addDigits(ans);
+
+    
+    /// reducing till we get single digit 
+        //ans
+}
+public int summission(int i, int sum) {
+        if (i == 0) {
+            return sum;
+        }
+        sum += (i % 10);
+        return summission(i / 10, sum);
+
+    }
+
+}
+{
+    
+}
